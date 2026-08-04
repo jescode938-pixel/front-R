3eca4084dbaa548c24acab406621778b9f172dd3
Sistemas <jescode938@gmail.com>
2026-08-04 17:22:14 -0500

    actualizacion api front

--- src/Api/Config/apiConfig.js
+++ src/Api/Config/apiConfig.js
@@ -1,3 +1,3 @@
-//export const Base_url = 'https://refrielectric-back.vercel.app/api';
+export const Base_url = 'https://back-r-six.vercel.app/api';
 
-export const Base_url = 'http://localhost:3000/api';
+//export const Base_url = 'http://localhost:3000/api';
